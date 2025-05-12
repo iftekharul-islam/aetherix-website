aa0f6a30a614c1190fbfddd80b64f6fef21c9fef
unknown <mamunhowlader707@gmail.com>
2025-05-13 01:56:30 +0600

    working on navigation bar and other fixations

--- src/components/global/Header.js
+++ src/components/global/Header.js
@@ -1,21 +1,39 @@
-import Link from "next/link";
+import Link from 'next/link';
+import Navbar from '../navbar/Navbar';
 
 const Header = () => {
     return (
-        <header id="siteHeader" className="fixed top-0 left-0 w-full z-50 transition-all duration-300 bg-gradient-to-r from-[#002a57] to-[#0097b2]">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex justify-between items-center h-16">
-                <div className="text-white text-xl font-bold">Logosipsum</div>
-                <nav className="space-x-6 text-white">
-                    <Link href="/" className="hover:text-gray-200">Home</Link>
-                    <Link href="/about" className="hover:text-gray-200">About</Link>
-                    <Link href="/services" className="hover:text-gray-200">Services</Link>
-                    <Link href="/blogs" className="hover:text-gray-200">Blog</Link>
-                    <Link href="/contact" className="hover:text-gray-200">Contact</Link>
-                </nav>
+        /*   <header
+            id='siteHeader'
+            className='fixed top-0 left-0 w-full z-50 transition-all duration-300 bg-gradient-to-r from-[#002a57] to-[#0097b2]'
+        >
+            <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+                <div className='flex justify-between items-center h-16'>
+                    <div className='text-white text-xl font-bold'>Logosipsum</div>
+                    <nav className='space-x-6 text-white'>
+                        <Link href='/' className='hover:text-gray-200'>
+                            Home
+                        </Link>
+                        <Link href='/about' className='hover:text-gray-200'>
+                            About
+                        </Link>
+                        <Link href='/services' className='hover:text-gray-200'>
+                            Services
+                        </Link>
+                        <Link href='/blogs' className='hover:text-gray-200'>
+                            Blog
+                        </Link>
+                        <Link href='/contact' className='hover:text-gray-200'>
+                            Contact
+                        </Link>
+                    </nav>
                 </div>
             </div>
-        </header>
+            <Navbar />
+        </header> */
+        <>
+            <Navbar isHomePage={true} />
+        </>
     );
-}
+};
 export default Header;
