d763fd9aba2e425810d7367b012b24d3decb0c07
unknown <mamunhowlader707@gmail.com>
2025-06-28 01:06:03 +0600

    blog section and contact section done

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -4,16 +4,16 @@ import React from 'react';
 import { blogDetails } from '@/data/blogDetails';
 function BlogId({ params }) {
     const { slug } = params;
-    const blogData = blogDetails[slug];
+    /*    const blogData = blogDetails[slug];
 
     if (!blogData) {
         return <div>Blog post not found</div>;
-    }
+    } */
 
     return (
         <div>
             <HeaderSection />
-            <BlogDetails blog={blogData} />
+            <BlogDetails />
         </div>
     );
 }
--- src/components/blogs/BlogDetails.jsx
+++ src/components/blogs/BlogDetails.jsx
@@ -1,10 +1,7 @@
 'use client';
-import { useState } from 'react';
-import { ChevronRight, Home, Menu, X, Facebook, Twitter, Instagram, Linkedin } from 'lucide-react';
 import blog1 from '@/assets/blog1.jpg';
-import team4 from '@/assets/team4.jpg';
 import Image from 'next/image';
-import Link from 'next/link';
+import SocialShare from './SocialShare';
 function BlogDetails({ blog }) {
     return (
         <>
@@ -110,35 +107,7 @@ function BlogDetails({ blog }) {
                         </span>
                     </div>
 
-                    <div className='flex items-center space-x-3'>
-                        <span className='text-sm text-sm font-bold text-black'>SHARE</span>
-                        <div className='flex space-x-2'>
-                            <Link
-                                href='#'
-                                className='p-2 bg-secondary text-white rounded-full hover:bg-blue-700 transition-colors cursor-pointer'
-                            >
-                                <Facebook size={16} />
-                            </Link>
-                            <Link
-                                href='#'
-                                className='p-2 bg-secondary text-white rounded-full hover:bg-sky-600 transition-colors cursor-pointer'
-                            >
-                                <X size={16} />
-                            </Link>
-                            <Link
-                                href='#'
-                                className='p-2 bg-secondary text-white rounded-full hover:bg-pink-700 transition-colors cursor-pointer'
-                            >
-                                <Instagram size={16} />
-                            </Link>
-                            <Link
-                                href='#'
-                                className='p-2 bg-secondary text-white rounded-full hover:bg-blue-800 transition-colors cursor-pointer'
-                            >
-                                <Linkedin size={12} />
-                            </Link>
-                        </div>
-                    </div>
+                    <SocialShare />
                 </div>
             </div>
         </>
